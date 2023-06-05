b6fa211753600d7b6edfb7e13651678b246fecc0
thissound22 <22011837@sju.ac.kr>
2023-06-05 20:03:28 +0900

    TCA 패키지 추가

---
+++ finiens/finiens/finiensApp.swift
@@ -0,0 +1,17 @@
+//
+//  finiensApp.swift
+//  finiens
+//
+//  Created by 이소리 on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct finiensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
