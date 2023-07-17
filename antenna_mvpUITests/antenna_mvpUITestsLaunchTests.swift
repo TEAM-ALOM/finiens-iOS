62a1b3a5af8bde730811c5656a13d8d159eef646
hwiseon_9151 <hwiseon_9151@naver.com>
2023-07-17 23:31:25 +0900

    Initial Commit

---
+++ antenna_mvpUITests/antenna_mvpUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  antenna_mvpUITestsLaunchTests.swift
+//  antenna_mvpUITests
+//
+//  Created by 김휘선 on 2023/07/17.
+//
+
+import XCTest
+
+final class antenna_mvpUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
