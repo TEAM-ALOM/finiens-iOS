7b7391e82e1fc5fc5ac7599c006c04f64c6c44f0
hwiseon_9151 <hwiseon_9151@naver.com>
2023-07-17 23:31:25 +0900

    Initial Commit

---
+++ antenna_mvp/antenna_mvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  antenna_mvpApp.swift
+//  antenna_mvp
+//
+//  Created by 김휘선 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct antenna_mvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
