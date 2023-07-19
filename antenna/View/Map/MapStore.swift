35d5d952aa938b6c68bd0fcc4be0738f757478cc
thissound22 <22011837@sju.ac.kr>
2023-07-19 20:45:53 +0900

    에러 해결

--- antenna/View/Map/MapStore.swift
+++ antenna/View/Map/MapStore.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+struct MapStore: ReducerProtocol {
+    // State
+    struct State: Equatable {
+        var Location: String = ""
+    }
+    
+    // Action
+    enum Action: Equatable {
+        case locationSearchBarTapped
+        case zoomInButtonTapped
+        case zoomOutButtonTapped
+        case routeSearchButtonTapped
+        case myLocationButtonTapped
+        case homeTapped
+        case mapTapped
+        case menuTapped
+    }
+    
+    // Environment
+    
+    // Reducer
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .locationSearchBarTapped:
+                // 장소 검색 뷰로 전환
+                return .none
+                
+            case .zoomInButtonTapped:
+                // 화면 확대
+                return .none
+                
+            case .zoomOutButtonTapped:
+                // 화면 축소
+                return .none
+                                
+            case .routeSearchButtonTapped:
+                // 출발지-도착지 검색창으로 변경
+                // x버튼으로 변경
+                return .none
+                
+            case .myLocationButtonTapped:
+                // 현재 내 위치로 이동
+                return .none
+                
+            case .homeTapped:
+                // 홈 뷰로 전환
+                return .none
+                
+            case .mapTapped:
+                // 현재 뷰 -> 변경사항 없음
+                return .none
+                
+            case .menuTapped:
+                // 메뉴 뷰로 전환
+                return .none
+            }
+        }
+    }
+}
--- antenna/View/Map/MapView.swift
+++ antenna/View/Map/MapView.swift
@@ -6,15 +6,111 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct MapView: View {
+
+    // Store
+    let store: StoreOf<MapStore>
+    
+    init(store: StoreOf<MapStore>) {
+        self.store = store
+        self.viewStore = ViewStore(self.store.scope(state: ViewState.init(state:)))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // 장소 검색 바
+        WithViewStore(self.store, observe: { $0 }) { viewStore in
+            VStack {
+                ForEachStore(self.store.scope(
+                    state: \.Location,
+                    action: MapStore.Action.textchange(id: action:))) {
+                        LocationSearchView(store: $0)
+                    }
+                Button {
+                    viewStore.send(.searchingLocationView)
+                } label: {
+                    Text("장소 검색")
+                    Spacer()
+                    Image("magnifyingglass")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                }
+                // fullScreenCover or SearchingLocationView
+
+                .cornerRadius(10)
+                .foregroundColor(.gray)
+                
+                // 버튼들 생성만
+                HStack {
+                    VStack {
+                        Button {
+                            Image("plus.circle.fill")
+                        }
+                        Button {
+                            Image("minus.circle.fill")
+                        }
+                    }
+                    Spacer()
+                    
+                    VStack {
+                        Button {
+                            Image("arrow.triangle.up.right.circle.fill")
+                        }
+                        Button {
+                            // 현재 위치 이미지
+                        }
+                    }
+                }
+                    
+            }
+            .padding()
+            .onAppear{ viewStore.send(.onAppear) }
+        }
+    }
+}
+
+// 지도 화면이긴 한데-root뷰 같은 데서 한번에 묶으면 좋을 듯
+//TabView {
+//    // 홈
+//    VStack {
+////                    HomeView()
+//    }
+//    .tabItem {
+//        Text("홈")
+//    }
+//    // 지도
+//
+//    .tabItem {
+//        Text("지도")
+//    }
+//    // 메뉴
+//    VStack {
+////                    MenuView
+//    }
+//    .tabItem {
+//        Text("메뉴")
+//    }
+//}
+
+struct LocationSearchView: View {
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        NavigationStack {
+            Text("\(searchText)")
+        }
+        .searchable(text: $searchText, prompt: "장소 검색")
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(store:
+                    Store(
+                        initialState: MapStore.State,
+                        reducer: MapStore()
+                         )
+        )
     }
 }
